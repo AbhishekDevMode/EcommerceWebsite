441484755a4766424a186db3f1e6e208c6276dca
AbhishekDevMode <itsabhishek5678@gmail.com>
2026-06-09 18:22:20 +0530

    Product Repository added

---
+++ src/main/java/com/ecom/EcomProject/controller/CategoryController.java
@@ -0,0 +1,25 @@
+package com.ecom.EcomProject.controller;
+
+import java.util.List;
+
+import com.ecom.EcomProject.model.Category;
+import com.ecom.EcomProject.service.CategoryService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/categories")
+public class CategoryController {
+    private CategoryService categoryService;
+
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    @GetMapping
+    public List<Category> getAllCategories() {
+        return categoryService.getAllCategories();
+    }
+
+}
