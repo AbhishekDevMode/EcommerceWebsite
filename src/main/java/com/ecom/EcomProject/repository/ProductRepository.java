9594f4ce53e3e5e7d5e827b086902089040ea2a8
AbhishekDevMode <itsabhishek5678@gmail.com>
2026-06-09 18:31:45 +0530

    ProductService fil added

--- src/main/java/com/ecom/EcomProject/repository/ProductRepository.java
+++ src/main/java/com/ecom/EcomProject/repository/ProductRepository.java
@@ -1,4 +1,12 @@
 package com.ecom.EcomProject.repository;
 
-public interface ProductRepository {
+import com.ecom.EcomProject.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    List<Product> findByCategoryId(Long categoryId);
 }
