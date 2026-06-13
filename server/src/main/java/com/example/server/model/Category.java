2278392fb7f617525dbb24a0903744a5d31a0315
AbhishekDevMode <itsabhishek5678@gmail.com>
2026-06-13 16:25:42 +0530

    models added

---
+++ server/src/main/java/com/example/server/model/Category.java
@@ -0,0 +1,27 @@
+package com.example.server.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Generated;
+import lombok.ToString;
+
+import java.util.Set;
+
+@Entity
+@Data
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    @JsonIgnoreProperties("category")
+    private Set<Product> products;
+
+
+}
---
+++ server/src/main/java/com/example/server/model/Product.java
@@ -0,0 +1,48 @@
+package com.example.server.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+@Entity
+@Data
+public class Product {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+
+    @Column(name="imageurl")
+    private String imageUrl;
+
+    private Double price;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id", nullable = false)
+    @JsonIgnoreProperties("products")
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    private Category category;
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {  this.name = name; }
+    public String getDescription() {return description;}
+    public void setDescription(String description) { this.description = description; }
+    public String getImageUrl() {return imageUrl;}
+    public void setImageUrl(String imageUrl) {this.imageUrl = imageUrl;}
+    public Double getPrice() {return price;}
+    public void setPrice(Double price) {this.price = price;}
+    public Category getCategory() {return category;}
+    public void setCategory(Category category) {this.category = category;}
+
+
+}
