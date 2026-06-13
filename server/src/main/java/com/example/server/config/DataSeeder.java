d35f713ada3b525ea346fb57c46c4a2849b1bd02
AbhishekDevMode <itsabhishek5678@gmail.com>
2026-06-13 16:23:09 +0530

    controllers added

--- server/src/main/java/com/example/server/config/DataSeeder.java
+++ server/src/main/java/com/example/server/config/DataSeeder.java
@@ -1,4 +1,68 @@
 package com.example.server.config;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import java.util.Arrays;
 
-public class DataSeeder {
+@Component
+public class DataSeeder implements CommandLineRunner {
+
+    private final ProductRepository productRepository;
+    private final CategoryRepository categoryRepository;
+
+    public DataSeeder(ProductRepository productRepository, CategoryRepository categoryRepository) {
+        this.productRepository = productRepository;
+        this.categoryRepository = categoryRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        //Clear all existing data this clears the existing data and repopulate the data
+        productRepository.deleteAll();
+        categoryRepository.deleteAll();
+
+        //Create Categories
+        Category electronics = new Category();
+        electronics.setName("Electronics");
+
+        Category clothing = new Category();
+        clothing.setName("Clothing");
+
+        Category home = new Category();
+        home.setName("Home and Kitchen");
+
+        categoryRepository.saveAll(Arrays.asList(electronics, home, clothing));
+
+        //Create Products
+        Product phone = new Product();
+        phone.setName("SmartPhone");
+        phone.setDescription("Latest model smartphone with amazing features.");
+        phone.setImageUrl("/images/smartphone.jpeg");
+        phone.setPrice(699.99);
+        phone.setCategory(electronics);
+
+        Product laptop=new Product();
+        laptop.setName("Laptop");
+        laptop.setDescription("High-performance laptop for work and play");
+        laptop.setImageUrl("/images/laptop.jpeg");
+        laptop.setPrice(999.99);
+        laptop.setCategory(electronics);
+
+        Product jacket=new Product();
+        jacket.setName("Winter Jacket");
+        jacket.setDescription("Warm and cozy jacket for winter.");
+        jacket.setImageUrl("/images/jacket.jpeg");
+        jacket.setPrice(129.99);
+        jacket.setCategory(clothing);
+
+        Product blender=new Product();
+        blender.setName("Blender Machine");
+        blender.setDescription("Both for juice and grinding");
+        blender.setImageUrl("/images/blender.jpeg");
+        blender.setPrice(89.99);
+        blender.setCategory(home);
+
+        productRepository.saveAll(Arrays.asList(phone,laptop,jacket,blender));
+
+    }
 }
---
+++ server/src/main/java/com/example/server/controllers/CategoryController.java
@@ -0,0 +1,23 @@
+package com.example.server.controllers;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/categories")
+@CrossOrigin(origins="http://localhost:5173")
+public class CategoryController {
+    private CategoryService categoryService;
+
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    @GetMapping
+    public List<Category> getAllCategories() {
+        return categoryService.getAllCategories();
+    }
+
+}
